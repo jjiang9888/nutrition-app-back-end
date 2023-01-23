99d73541b95fddbe0d22befdd99dac8ea7be301b
hairyElephant <josecalderon353@gmail.com>
2023-01-23 10:11:06 -0500

    clean up

--- DB/connection.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose"
-
-mongoose.set('returnOriginal', false)
-
-mongoose.connect("mongodb://127.0.0.1:27017/Nutrition-api").catch((err) => {console.log(`Error connection to MongoDB: ${err.message}`)})
-
-
-export default mongoose.connection;
