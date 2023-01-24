e82605bf14b1fc0ea17403c2e59d86134aca30b9
Ron Lanzilotta <rlanzilotta1010@gmail.com>
2023-01-23 21:21:19 -0500

    fixed json and added Calorie and Notes keys to Models + data.js

--- Models/Item.js
+++ Models/Item.js
@@ -3,10 +3,12 @@ const Schema = mongoose.Schema;
 
 let Item = new Schema({
     Name: String,
+    Calories: Number,
     Carbs: Number,
     Fats: Number,
     Proteins: Number,
-    Quantity: Number
+    Quantity: Number,
+    Notes: String
 },{timestamps: true})
 
 export default mongoose.model("items", Item)
